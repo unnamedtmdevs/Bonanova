e986a98c5b8d3ffe537eace3d0a3084669def9f9
ludik <bakhanetssimon@gmail.com>
2026-02-26 13:55:22 +0300

    Initial Commit

---
+++ Bonanova/BonanovaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonanovaApp.swift
+//  Bonanova
+//
+//  Created by Simon Bakhanets on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct BonanovaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bonanova/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bonanova
+//
+//  Created by Simon Bakhanets on 26.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
